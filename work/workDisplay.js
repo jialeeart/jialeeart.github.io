c351e95e35b433101f1699e7db8a8f6f29f61490
Ryan Tong <rrtong@ucdavis.edu>
2019-10-19 00:56:33 -0700

    add way to close image after opening

--- work/workDisplay.js
+++ work/workDisplay.js
@@ -2,6 +2,14 @@ function fullDisplay(folderName, imageName) {
   let oldFullDisplayImage = document.querySelector("#fullDisplay img");
   let fullDisplayImage = document.createElement("img");
   fullDisplayImage.src = `../images/${folderName}/full/${imageName}`;
+  fullDisplayImage.setAttribute('onclick', 'closeImage()');
   document.querySelector("#fullDisplay").replaceChild(fullDisplayImage, oldFullDisplayImage);
+  $('html, body').animate({ scrollTop: 0 }, 'fast');
+}
+
+function closeImage() {
+  let oldFullDisplayImage = document.querySelector("#fullDisplay img");
+  let closeFullDisplayImage = document.createElement("img");
+  document.querySelector("#fullDisplay").replaceChild(closeFullDisplayImage, oldFullDisplayImage);
   $('html, body').animate({ scrollTop: 0 }, 'fast');
 }
